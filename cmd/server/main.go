1b6fbe7e807c9e4d6a8c5a7251f04898dd7c7f4f
msrexe <mlheymen.ms@gmail.com>
2022-02-07 01:17:31 +0300

    Retry mechanism added for postgresql connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/coineus/coineus-server/internal/config"
 	"github.com/coineus/coineus-server/internal/router"
@@ -15,6 +17,26 @@ var (
 
 func main() {
 	dbCfg := os.Getenv("POSTGRE_DB_URI")
+
+	retryTime, err := strconv.Atoi(os.Getenv("RETRY_COOLDOWN"))
+	if err != nil || os.Getenv("RETRY_COOLDOWN") == "" {
+		retryTime = 5
+	}
+
+	retryCount, err := strconv.Atoi(os.Getenv("RETRY_COUNT"))
+	if err != nil || os.Getenv("RETRY_COUNT") == "" {
+		retryCount = 5
+	}
+
+	// Waiting for other dependencies to be ready
+	for i := 0; i < retryCount; i++ {
+		if _, err := storage.CreatePool(dbCfg); err != nil {
+			log.Println("Error connecting to postgres: ", err)
+			time.Sleep(time.Second * time.Duration(retryTime))
+		} else {
+			break
+		}
+	}
 
 	db, err := storage.CreatePool(dbCfg)
 	if err != nil {

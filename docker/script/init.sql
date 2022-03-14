8ab1e6d02e8b686da438f5dd431be8cf3f6d9d93
msrexe <mlheymen.ms@gmail.com>
2022-03-14 22:53:19 +0300

    postgre fcking sql finally fixed

--- docker/script/init.sql
+++ docker/script/init.sql
@@ -1,45 +1,40 @@
-CREATE TABLE users (
-    hash_id   VARCHAR(500) NOT NULL,
+CREATE TABLE IF NOT EXISTS users (
+    hash_id   VARCHAR(500) NOT NULL PRIMARY KEY,
     username    VARCHAR(100) NOT NULL,
     created_at     DATE NOT NULL,
     password_hash  VARCHAR(500) NOT NULL,
-    email  VARCHAR(300) NOT NULL,
-   CONSTRAINT users PRIMARY KEY (hash_id)
+    email  VARCHAR(300) NOT NULL
 );
 
-CREATE TABLE wallets (
-    hash_id   VARCHAR(500) NOT NULL,
+CREATE TABLE IF NOT EXISTS wallets (
+    hash_id   VARCHAR(500) NOT NULL PRIMARY KEY,
     user_id   VARCHAR(500) NOT NULL,
-    name   VARCHAR(100) NOT NULL,
-   CONSTRAINT wallets PRIMARY KEY (hash_id)
+    name   VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE wallet_operations (
-    hash_id   VARCHAR(500) NOT NULL,
+CREATE TABLE IF NOT EXISTS wallet_operations (
+    hash_id   VARCHAR(500) NOT NULL PRIMARY KEY,
     wallet_id  VARCHAR(500) NOT NULL,
-    operation_id VARCHAR(500) NOT NULL,
-   CONSTRAINT wallet_operations PRIMARY KEY (hash_id)
+    operation_id VARCHAR(500) NOT NULL
 );
 
-CREATE TABLE archived_operations (
-    hash_id   VARCHAR(500) NOT NULL,
+CREATE TABLE IF NOT EXISTS archived_operations (
+    hash_id   VARCHAR(500) NOT NULL PRIMARY KEY,
     user_id  VARCHAR(500) NOT NULL,
     archived_at DATE NOT NULL,
     coin_symbol VARCHAR(100) NOT NULL,
     coin_amount DOUBLE PRECISION NOT NULL,
     buy_cost DOUBLE PRECISION NOT NULL,
-    sell_cost DOUBLE PRECISION NOT NULL,
-    CONSTRAINT archived_operations PRIMARY KEY (hash_id)
+    sell_cost DOUBLE PRECISION NOT NULL
 );
 
 
-CREATE TABLE recent_operations (
-    hash_id   VARCHAR(500) NOT NULL,
+CREATE TABLE IF NOT EXISTS recent_operations (
+    hash_id   VARCHAR(500) NOT NULL PRIMARY KEY,
     user_id  VARCHAR(500) NOT NULL,
     created_at DATE NOT NULL,
     coin_symbol VARCHAR(100) NOT NULL,
     coin_amount DOUBLE PRECISION NOT NULL,
-    buy_cost DOUBLE PRECISION NOT NULL,
-    CONSTRAINT recent_operations PRIMARY KEY (hash_id)
+    buy_cost DOUBLE PRECISION NOT NULL
 );
 
